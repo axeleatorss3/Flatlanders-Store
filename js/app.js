01e7e66142d0791917867e08699941c5a3ef52b8
Abraham Luna <abraham11999@hotmail.com>
2020-12-14 14:00:04 -0700

    Directivas y Controladores

--- js/app.js
+++ js/app.js
@@ -1,21 +1,37 @@
 (function () {
-   let app = angular.module('fsStore', [
+    let app = angular.module('fsStore', [
         /* 1. Main App Module */
         'ui.router',
         'ui.router.state.events',
-        'landing'
-        
+        'landing',
+        'navbar',
+        'footer',
+        'products'
     ]);
 
-    app.config(['$stateProvider','$urlRouterProvider','$locationProvider',function($stateProvider,$urlRouterProvider,$locationProvider){
-        $stateProvider.state('landing',{
-            url: '/landing',
-            templateUrl: 'partials/landing.html',
-            controller: 'landing'
-        });
+    app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function ($stateProvider, $urlRouterProvider, $locationProvider) {
+        $stateProvider.state('landing', {
+                url: '/landing',
+                templateUrl: 'partials/landing.html',
+                controller: 'landing'
+            })
+            .state('app', {
+                url: '/app',
+                abstract: true,
+                templateUrl: 'partials/main-panel.html',
+                controller: 'App'
+            })
+            .state('app.products', {
+                url: '/products',
+                views: {
+                    'content': {
+                        templateUrl: 'partials/products/products.html',
+                        controller: 'Products'
+                    }
+                }
+            });
 
         $urlRouterProvider.otherwise('landing');
         $locationProvider.html5Mode(false);
     }]);
-
 })();
---
+++ js/controllers/appController.js
@@ -0,0 +1,9 @@
+(function () {
+    let app = angular.module('app', [
+
+    ]);
+
+    app.controller('App', ['$scope', '$state', function ($scope, $state) {
+
+    }])
+})();
