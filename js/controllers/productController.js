1d15208b6af88e0beedc47da84e686425b9cda02
Abraham Luna <abraham11999@hotmail.com>
2020-12-14 20:34:33 -0700

    Implementación de Tabs

--- js/controllers/productController.js
+++ js/controllers/productController.js
@@ -1,27 +1,39 @@
-(function(){
-'use strict';
+(function () {
+    'use strict';
 
-let app = angular.module('product', [] )
+    let app = angular.module('product', [])
 
-app.controller('Product',  ['$scope', '$state', '$stateParams', '$http', 
-function ($scope, $state, $stateParams, $http) {
-    let product = this;
+    app.controller('Product', ['$scope', '$state', '$stateParams', '$http',
+        function ($scope, $state, $stateParams, $http) {
+            let product = this;
 
-    product._id = $stateParams._id;
-    product.detail = {};
-    product.reviews = [];
-    product.sumStars = 0;
-    product.avgStars = 0;
-    $http.get("./dummy/products.json").then(function(data) {
-        product.products = data.data.gems;
+            product._id = $stateParams._id;
+            product.detail = {};
+            product.reviews = [];
+            product.sumStars = 0;
+            product.avgStars = 0;
 
-        product.detail = product.products.find(p=> p._id === product._id);
-        product.reviews = product.detail.reviews;
-        product.sumStars = product.reviews.map(item => item.stars).reduce((prev, next) => prev + next);
-        product.avgStars = product.sumStars / product.products.length;
-        console.log("sum", product.sumStars);
-        console.log("sum", product.avgStars);
-    });
+            product.current = 0;
 
-}]);
+            product.setCurrent = function(i){
+                product.current = i || 0;
+            }
+
+            product.isSet = function(i){
+                return i === product.current;
+            }
+
+            $http.get("./dummy/products.json").then(function (data) {
+                product.products = data.data.gems;
+
+                product.detail = product.products.find(p => p._id === product._id);
+                product.reviews = product.detail.reviews;
+                product.sumStars = product.reviews.map(item => item.stars).reduce((prev, next) => prev + next);
+                product.avgStars = product.sumStars / product.products.length;
+                console.log("sum", product.sumStars);
+                console.log("sum", product.avgStars);
+            });
+
+        }
+    ]);
 })();
